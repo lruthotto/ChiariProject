dd6dcecc6d6c533781c5e5a84cf907e530c498c1
Emma <eghart@colgate.edu>
2021-06-23 15:31:26 -0400

    Revised Options, Created .mat file option
    Revised options available in histnormal, adding an option to create a mat file with normalized and original (max-adjusted, uint8) images

--- histnormal.m
+++ histnormal.m
@@ -1,7 +1,11 @@
-function histnormal(refids, opt)
+function histnormal(refids, figs, jpgs, mat)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-% Ex:       histnormal([1 2 3 4])   will create 3 figures:
+% refids = patient id numbers       vector
+% figs   = choice for figures       0 or 1
+% jpgs   = choice for .jpgs         0 or 1
+% mat    = choice for .mat file     0 or 1
+%
+% Ex:       histnormal([1 2 3 4], 1, 1, 1)  will create 3 figures:
 %
 %           Figure 1) Original Images
 %                     Histograms of Original Images
@@ -11,80 +15,94 @@ function histnormal(refids, opt)
 %                     Cumulative Distributions of Adjusted Images
 %           Figure 3) Original Images
 %                     Adjusted Images
-%           
-%           histnormal([1 2 3 4],1) will do the same, and also print the
-%           original and adjusted image as original# and adjusted#,
-%           respectively, with # corresponding to the image's id number
 %
-% refids = vector of patient id numbers
+%           It will also print each original and adjusted image to a .jpg
+%           file (original#.jpg and adjusted#.jpg) and will store the
+%           images in a .mat file (images_normal and images_adjusted, each 
+%           256x256xlength(refids) double)
+%
+%           defaults with figures on and .jpg/.mat printing off
 %
 %  NOTE: this function lines images up side by side in one figure, so doing
 %  inputting more than about 5 ids at once (in a vector) is not recommended
 %
 % Uses chiariTrainingData.mat and MATLAB's Image Processing Toolbox
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% 
-if nargin<2 || isempty(opt)
-     opt=0;
+%% Defaults
+if nargin<4 || isempty(mat)
+     mat=0;
+end
+if nargin<3 || isempty(jpgs)
+     jpgs=0;
+end
+if nargin<2 || isempty(figs)
+     figs=1;
+end
+if nargin<1 || isempty(refids)
+     refids=1:5;
 end
 %%
 N = length(refids);
 data = load('chiariTrainingData.mat');
 images = data.imagesTrain;
 
-%% First figure: 
-figure(1)
-clf;
-for i=1:N
-    image = uint8(256 .* (images(:,:,refids(i)) ./ ...  %max adjusted image
-        max(max(images(:,:,refids(i))))));
-    hold on
-    subplot(3,N,i)
-    imagesc(image)
-    axis equal
-    axis off
-    colormap gray
-    subplot(3,N,i+N)                    %one row below
-    histogram(image)
-    subplot(3,N,i+2*N)                  %two rows below
-    cdfplot(reshape(image,[256*256 1]))
-end
-max(max(images(:,:,refids(i))))
-figure(2)
-clf;
-%% Second Figure: Equalizing Histogram and Displaying Adjusted Images
-for i=1:N
-    image = uint8(256 .* (images(:,:,refids(i)) ./ ...
-        max(max(images(:,:,refids(i))))));
-    hold on
-    subplot(3,N,i)
-    histeq(image)
-    axis equal
-    axis off
-    colormap gray
-    subplot(3,N,i+N)
-    histogram(histeq(image))
-    subplot(3,N,i+2*N)
-    cdfplot(reshape(histeq(image),[256*256 1]))
-end
-%% Third Figure: Comparing Original and Adjusted Images
-figure(3)
-clf;
-for i=1:N
-    image = uint8(256 .* (images(:,:,refids(i)) ./ ...
-        max(max(images(:,:,refids(i))))));
-    hold on
-    subplot(2,N,i)
-    imagesc(image)
-    axis equal
-    axis off
-    colormap gray
-    subplot(2,N,i+N)
-    histeq(image)
+%% Figures
+if figs == 1
+    % First figure: Original Images and Histograms
+    figure(1)       
+    clf;
+    for i = 1:N
+        image = uint8(256 .* (images(:,:,refids(i)) ./ ...  %max adjusted image
+            max(max(images(:,:,refids(i))))));
+        hold on
+        subplot(3,N,i)
+        imagesc(image)
+        axis equal
+        axis off
+        colormap gray
+        subplot(3,N,i+N)                    %one row below
+        histogram(image)
+        subplot(3,N,i+2*N)                  %two rows below
+        cdfplot(reshape(image,[256*256 1]))
+    end
+    % Second Figure: Equalizing Histogram and Displaying Adjusted Images
+    figure(2)
+    clf;
+    for i = 1:N
+        image = uint8(256 .* (images(:,:,refids(i)) ./ ...
+            max(max(images(:,:,refids(i))))));
+        hold on
+        subplot(3,N,i)
+        histeq(image)
+        axis equal
+        axis off
+        colormap gray
+        subplot(3,N,i+N)
+        histogram(histeq(image))
+        subplot(3,N,i+2*N)
+        cdfplot(reshape(histeq(image),[256*256 1]))
+    end
+    % Third Figure: Comparing Original and Adjusted Images
+    figure(3)
+    clf;
+    for i = 1:N
+        image = uint8(256 .* (images(:,:,refids(i)) ./ ...
+            max(max(images(:,:,refids(i))))));
+        hold on
+        subplot(2,N,i)
+        imagesc(image)
+        axis equal
+        axis off
+        colormap gray
+        colorbar
+        subplot(2,N,i+N)
+        histeq(image)
+        colorbar
+    end
 end
 %% Printing Images as .jpgs
-if opt == 1
-    for i=1:N
+if jpgs == 1
+    for i = 1:N
         image = uint8(256 .* (images(:,:,refids(i)) ./ ...     %max adjusted image
             max(max(images(:,:,refids(i))))));
         filename1 = sprintf('%s%d.jpg','original',refids(i));  %filename for original/max-adjusted image
@@ -93,3 +111,19 @@ if opt == 1
         imwrite(histeq(image), filename2)
     end
 end
+%% Creating a .mat file with the new images
+if mat == 1
+    images_original = zeros(256,256,N);
+    images_normal = zeros (256,256,N);
+    for i = 1:N
+       image = uint8(256 .* (images(:,:,refids(i)) ./ ...     %max adjusted image
+            max(max(images(:,:,refids(i))))));
+       images_original(:,:,i) = image;
+       images_normal(:,:,i) = histeq(image);
+       
+    end
+    clear figs; clear jpgs; clear mat; clear N; clear refids; clear image; ... 
+        clear images; clear data; clear i; clear filename1; clear filename 2;
+    save('normalizedChiariTraining.mat')
+end
+
