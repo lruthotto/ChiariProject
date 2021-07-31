ae477c35b69717b30eb6af28c9a8aa73cd2147a4
Emma <eghart@colgate.edu>
2021-07-31 12:09:09 -0400

    Got rid of unused border option

--- biomarker.m
+++ biomarker.m
@@ -7,28 +7,21 @@ function [biom_bs, biom_cb] = biomarker(mask, dense, varargin)
 %               2: brainstem
 %     dense = 256x256 matrix of peakDisplacement values
 % dense_id  = the id number for the DENSE image of interest
-% options to include border, see figures, and display calculated biomarkers
+% options to see figures and display calculated biomarkers
 %% 
 % Establishing Defaults
-border = 0;
 figs = 0;
 display = 1;
 for k=1:2:length(varargin)          % overwrite defaults  
     eval([varargin{k},'=varargin{',int2str(k+1),'};']);
 end
-if border == 1
-    border = blur(dense,1)<2.5;     % defines a border for pixels we will ignore from the average based on too high displacement
-    %imagesc(border)
-else
-    border = ones(256,256);
-end
 
 cb = mask == 1;                             % defining cerebellum
-dense_cb = dense .* cb .* border;           % finding pixels to average
+dense_cb = dense .* cb;                     % finding pixels to average
 biom_cb = mean(nonzeros(dense_cb), 'all');  % calculating average
 
 bs = mask==2;                               % defining brain stem
-dense_bs = dense .* bs .* border;           % finding pixels to average
+dense_bs = dense .* bs;                     % finding pixels to average
 biom_bs = mean(nonzeros(dense_bs), 'all');  % calculating average
 
 if figs == 1                %displaying figures of averaged pixels
@@ -42,21 +35,3 @@ if display==1               %displaying calculated biomarkers
     disp(['Cerebellum:' num2str(biom_cb) '   Brain Stem:' num2str(biom_bs)])
 end
 end
-
-function [output] = blur(A,w)
-[row col] = size(A);
-A=uint8(A);
-B=nan(size(A) + (2*w));
-B(w+1:end-w,w+1:end-w)=A;
-output = 0*A;
-for i=w+1:row+w
-  for j=w+1:col+w
-    tmp=B(i-w:i+w,j-w:j+w);
-    output(i-w,j-w)=mean(tmp(~isnan(tmp)));
-  end
-end
-output=uint8(output);
-end
-
-%% BLUR CODE SOURCE
-% https://www.mathworks.com/matlabcentral/answers/450356-how-can-blur-an-image
